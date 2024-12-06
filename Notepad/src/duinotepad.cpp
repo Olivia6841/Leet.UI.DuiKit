f8df5341a6b92e0bce0e7c0b77665b9560443f25
Olivia <OliviaIsTyping@Outlook.com>
2024-12-06 09:38:19 -0700

    Use wWinMain instead of WinMain because its 2024

--- Notepad/src/duinotepad.cpp
+++ Notepad/src/duinotepad.cpp
@@ -26,11 +26,16 @@
 using namespace DirectUI;
 using namespace Leet::UI::DuiKit::Notepad;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd
+)
 {
     UNREFERENCED_PARAMETER(hInstance);
     UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(nCmdShow);
+    UNREFERENCED_PARAMETER(nShowCmd);
 
     HRESULT hRes;
 
